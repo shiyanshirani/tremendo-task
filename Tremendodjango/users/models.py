1554c126369c957e72e2e49015661b3ad3109e3e
shiyanshirani <shiyan99s@gmail.com>
2021-06-19 18:01:12 +0530

    student, teacher one to many rltn added

--- Tremendodjango/users/models.py
+++ Tremendodjango/users/models.py
@@ -2,6 +2,16 @@ from django.db import models
 
 # Create your models here.
 
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+    phone_number = models.BigIntegerField()
+    # photo = models.ImageField(upload_to = "images/")          # pip3 install pillow
+    address = models.TextField(blank=True)
+    
+    def __str__(self):
+        return f"{self.name}, {self.email}, {self.phone_number}, {self.address}"
+
 class Student(models.Model):
     GENDER_FEMALE = 'F'
     GENDER_MALE = 'M'
@@ -9,23 +19,21 @@ class Student(models.Model):
         (GENDER_FEMALE, 'Female'),
         (GENDER_MALE, 'Male')
         )
-    
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=254)
     # photo = models.ImageField(upload_to = "images/")          # pip3 install pillow
     gender = models.CharField(max_length=1, choices=GENDER_OPTIONS)
     address = models.TextField(blank=True)
+    batch = models.ForeignKey(Teacher, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.name}, {self.email}, {self.gender}, {self.address}"
+        return f"{self.name}, {self.email}, {self.gender}, {self.address}, {self.batch}"
 
-class Teacher(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=254)
-    phone_number = models.BigIntegerField()
-    # photo = models.ImageField(upload_to = "images/")          # pip3 install pillow
-    address = models.TextField(blank=True)
-    batch = models.ForeignKey(Student, on_delete=models.CASCADE, null=True)
-    
+class Batch(models.Model):
+    teacher = models.CharField(max_length=100)
+    student = models.CharField(max_length=100)
+    total_classes = models.IntegerField() 
+    completed_classes = models.IntegerField()
+
     def __str__(self):
-        return f"{self.name}, {self.email}, {self.phone_number}, {self.address}, {self.batch}"
+        return f"{self.teacher}"
